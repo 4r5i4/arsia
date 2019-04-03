1cf8fad8f332650e32d2484a147e2351e172e035
Arsia Ardalan <parthiardalan@gmail.com>
2019-04-02 23:45:14 -0400

    adding routing

---
+++ src/Components/Contact/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact(props) {
+	return (
+		<div className="contact">
+			<p>CONTACTS</p>
+		</div>
+	);
+}
+
+export default Contact;
--- src/Components/Navigation-menu/navigationMenu.js
+++ src/Components/Navigation-menu/navigationMenu.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import About from '../About/about';
+import Resume from '../Resume/resume';
+import Contact from '../Contact/contact';
 
 import './styles.css';
 import NavigationButton from '../NavigationButton/navigationButton';
 
 const NavigationMenu = (props) => {
 	return (
-		<div className="navigation-menu">
-			{props.buttons.map((button) => {
-				return <NavigationButton name={button} key={button} />;
-			})}
-		</div>
+		<Router>
+			<div className="navigation-menu">
+				{props.buttons.map((button, index) => {
+					if (button != 'resume') {
+						return (
+							<Link to={`/${button}`} key={index}>
+								<NavigationButton name={button} />
+							</Link>
+						);
+					} else {
+						return (
+							<Link to="/" key={index}>
+								<NavigationButton name={button} />
+							</Link>
+						);
+					}
+				})}
+			</div>
+
+			<Switch>
+				<Route exact path="/" component={Resume} />
+				<Route path="/about" component={About} />
+				<Route path="/contact" component={Contact} />
+			</Switch>
+		</Router>
 	);
 };
 
